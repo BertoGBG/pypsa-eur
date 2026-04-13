8763c71b00e4d28869bd7eeace8d7fd5ed3d7cc9
BertoGBG <albertoalamia@gmail.com>
2026-04-13 13:05:17 +0200

    Add GenomeDK run files (AU and DTU profiles, scripts, READMEs)

---
+++ run_snakemake_DTU.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -eo pipefail
+
+REPO="/home/albal/workspace/pypsa-eur_AA/pypsa-eur"
+cd "$REPO"
+
+source "$HOME/anaconda3/etc/profile.d/conda.sh"
+
+echo "Starting Snakemake at $(date)"
+echo "Running on node: $(hostname)"
+echo "Working directory: $(pwd)"
+
+conda activate pypsa-eur
+
+# Run workflow
+snakemake --profile profiles/default_DTU \
+  --configfile config/config.default.yaml \
+  --rerun-incomplete \
+  all
+
+echo "Finished at $(date)"
